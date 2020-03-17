ce2b22e7a3d296ef76d22e3e15bab7b25bdcd937
Marissa <m4rsibar@gmail.com>
2020-03-16 23:53:39 -0400

    Calendar table added to database.  Weekly retrieval join with calendar to decipher any days without data.

--- app.py
+++ app.py
@@ -5,22 +5,24 @@ from datetime import datetime, timedelta
 from flask_cors import CORS
 
 
-# Init app
+# App
 app = Flask(__name__)
 CORS(app)
-# Database
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://grjibwwwdxokwx:f02ad6175a3472e6112f80ed7213b3fbff07f0ac2bf9e3bb186181e70b920d78@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d4icrnr6v2qbm3'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/MoodApp'
+
+# Database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://grjibwwwdxokwx:f02ad6175a3472e6112f80ed7213b3fbff07f0ac2bf9e3bb186181e70b920d78@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d4icrnr6v2qbm3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/MoodApp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+# Marshmallow
 ma = Marshmallow(app)
 
+
 # Mood Class
-
-
 class Mood(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
@@ -32,24 +34,34 @@ class Mood(db.Model):
         self.moodrating = moodrating
         self.comment = comment
 
+
+class Calendar(db.Model):
+
+    day_id = db.Column(db.Date, primary_key=True)
+    year = db.Column(db.SmallInteger, nullable=False)
+    month = db.Column(db.SmallInteger, nullable=False)
+    day = db.Column(db.SmallInteger, nullable=False)
+    day_of_week = db.Column(db.SmallInteger, nullable=False)
+    day_of_year = db.Column(db.SmallInteger, nullable=False)
+    week_of_year = db.Column(db.SmallInteger, nullable=False)
+
+
 # Mood Schema
-
-
 class MoodSchema(ma.Schema):
     class Meta:
         fields = ('id', 'date', 'moodrating', 'comment')
 
 
-# Init schema
 mood_schema = MoodSchema()
 moods_schema = MoodSchema(many=True)
 
 
+# Routes
 @app.route('/')
 def index():
-    return render_template("placeholder.html")
+    return render_template("index.html")
 
-# Routes
+
 @app.route('/mood', methods=['POST'])
 def add_mood():
     date = request.json['date']
@@ -81,13 +93,12 @@ def get_weeks_moods():
     start = str(start)
     end = str(end)
 
-    weekMoods = Mood.query.filter(
-        Mood.date >= start, Mood.date <= end).order_by(Mood.date)
-    result = moods_schema.dump(weekMoods)
 
-    x = [x['moodrating'] for x in result if 'moodrating' in x]
-    return(jsonify(x))
-    # return jsonify(result)
+# Raw sql requirement
+    data = db.session.execute(
+        f"SELECT m.id, m.date, coalesce(m.moodrating, 0) as moodrating, m.comment, c.day_of_week FROM mood m RIGHT JOIN calendar c ON m.date=c.day_id where c.week_of_year=11 and month=3 and year=2020 order by day_of_week")
+    return jsonify({'result': [dict(row) for row in data]})
+    # return jsonify(data)
 
 
 # Run server
