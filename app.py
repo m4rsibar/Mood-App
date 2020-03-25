408dac3642b1d43fd2b2c05c3f5c69aeddae7aa8
Marissa <m4rsibar@gmail.com>
2020-03-25 11:59:25 -0400

    Environment and variables set up.

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime
 from flask_cors import CORS
-
+import os
 
 # App
 app = Flask(__name__)
 CORS(app)
 
+if os.environ.get('PROD'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('LOCAL_URI')
 
-# Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://grjibwwwdxokwx:f02ad6175a3472e6112f80ed7213b3fbff07f0ac2bf9e3bb186181e70b920d78@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d4icrnr6v2qbm3'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/MoodApp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
-
 # Marshmallow
 ma = Marshmallow(app)
 
-
 # Mood Class
+
+
 class Mood(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
@@ -58,25 +60,32 @@ moods_schema = MoodSchema(many=True)
 # Routes
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("Forum.html")
 
 
 @app.route('/mood', methods=['POST'])
 def add_mood():
-    date = request.json['date']
-    moodrating = request.json['moodrating']
-    comment = request.json['comment']
+    date = request.form['date']
+    moodrating = request.form['moodrating']
+    moodratingDivided = int(moodrating)/10
+    comment = request.form['comment']
 
-    new_mood = Mood(date, moodrating, comment)
+    exists = db.session.query(db.exists().where(Mood.date == date)).scalar()
+    if(exists):
+        flash("You've already entered a mood for this date.")
+        return "fail"
+    else:
+        new_mood = Mood(date, moodratingDivided, comment)
+        db.session.add(new_mood)
+        db.session.commit()
+    return "success"
 
-    db.session.add(new_mood)
-    db.session.commit()
-
-    return mood_schema.jsonify(new_mood)
+    # return str(exists)
+    # return mood_schema.jsonify(new_mood)
 
 
 # Fetch all moods
-@app.route('/mood', methods=['GET'])
+@app.route('/getmoods', methods=['GET'])
 def get_moods():
     all_moods = Mood.query.all()
     result = moods_schema.dump(all_moods)
@@ -90,7 +99,6 @@ def get_weeks_moods():
     week = today.isocalendar()[1]
     month = today.month
     year = today.year
-
 
 # Raw sql requirement
     data = db.session.execute(
---
+++ static/forum.js
@@ -0,0 +1,64 @@
+var output = document.getElementById("output");
+
+var inputRange = document.getElementsByClassName("range")[0],
+  maxValue = 50,
+  speed = 5,
+  currValue,
+  rafID;
+
+inputRange.oninput = function() {
+  value = this.value / 10;
+  output.innerHTML = Math.round(value);
+};
+
+inputRange.min = 0;
+inputRange.max = maxValue;
+
+function animateHandler() {
+  // calculate gradient transition
+  var transX = currValue - maxValue;
+  // update input range
+  inputRange.value = currValue;
+  //Change slide thumb color on mouse up
+  if (currValue < 20) {
+    inputRange.classList.remove("ltpurple");
+  }
+  if (currValue < 40) {
+    inputRange.classList.remove("purple");
+  }
+  if (currValue < 60) {
+    inputRange.classList.remove("pink");
+  }
+
+  if (currValue > -1) {
+    window.requestAnimationFrame(animateHandler);
+  }
+
+  // decrement value
+  currValue = currValue - speed;
+}
+
+// move gradient
+inputRange.addEventListener("input", function() {
+  //Change slide thumb color on way up
+  if (this.value > 20) {
+    inputRange.classList.add("ltpurple");
+  }
+  if (this.value > 40) {
+    inputRange.classList.add("purple");
+  }
+  if (this.value > 60) {
+    inputRange.classList.add("pink");
+  }
+
+  //Change slide thumb color on way down
+  if (this.value < 20) {
+    inputRange.classList.remove("ltpurple");
+  }
+  if (this.value < 40) {
+    inputRange.classList.remove("purple");
+  }
+  if (this.value < 60) {
+    inputRange.classList.remove("pink");
+  }
+});
