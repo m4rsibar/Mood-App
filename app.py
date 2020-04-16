d0917bf1ae7bcd1a735dc5693f005e0c5f79352b
Marissa <m4rsibar@gmail.com>
2020-04-16 01:07:16 -0400

    working on dynamic URI functionality

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ import datetime
 from flask_cors import CORS
 import os
 
-
 app = Flask(__name__)
 CORS(app)
 
-if os.environ.get('PROD'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('LOCAL_URI')
-
+# if os.environ.get('PROD'):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# else:
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/MoodApp'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -56,6 +55,35 @@ mood_schema = MoodSchema()
 moods_schema = MoodSchema(many=True)
 
 
+def tableExists(tableName):
+    exists = db.session.execute(
+        f"SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = 'public'AND table_name = '{tableName}');"
+    )
+    return (exists.first()[0])
+
+
+def isTableEmpty(tableName):
+    IsEmpty = db.session.execute(
+        f"SELECT CASE WHEN EXISTS(SELECT 1 FROM {tableName}) THEN 0 ELSE 1 END;"
+    )
+    return (IsEmpty.first()[0])
+
+
+def FillCalendar():
+    db.session.execute(
+        f" INSERT INTO calendar (day_id, year, month, day, day_of_week, day_of_year, week_of_year)(SELECT ts,EXTRACT(YEAR FROM ts),EXTRACT(MONTH FROM ts),EXTRACT(DAY FROM ts),EXTRACT(DOW FROM ts),EXTRACT(DOY FROM ts),EXTRACT(WEEK FROM ts)FROM generate_series('2019-01-01'::timestamp, '2038-01-01', '1day'::interval) AS t(ts));"
+    )
+    db.session.commit()
+    print('populated')
+
+
+if tableExists('calendar') == True:
+    if isTableEmpty('calendar') == 1:
+        FillCalendar()
+    else:
+        print("didnt fill")
+
+
 # Routes
 @app.route('/', methods=['GET'])
 def week():
@@ -81,15 +109,13 @@ def get_moods():
 
 @app.route('/month', methods=['GET', 'POST'])
 def get_month_moods():
-    # mo = request.form['month']
-    # yr = request.form['year']
-
     dt = datetime.datetime.today()
     month = dt.month
     year = dt.year
 
     data = db.session.execute(
-        f"SELECT * FROM mood m JOIN calendar c ON m.date=c.day_id and month={month} and year={year} order by c.day ")
+        f"SELECT * FROM mood m JOIN calendar c ON m.date=c.day_id and month={month} and year={year} order by c.day "
+    )
     return jsonify({'result': [dict(row) for row in data]})
 
 
@@ -103,18 +129,19 @@ def get_weeks_moods():
 
     dt = datetime.datetime.today()
     week = dt.isocalendar()[1]
-    start = dt - datetime.timedelta(days=dt.weekday()+1)
+    start = dt - datetime.timedelta(days=dt.weekday() + 1)
     end = start + datetime.timedelta(days=6)
 
     if start.month != end.month:
-        month = (str(start.month)+","+str(end.month))
+        month = (str(start.month) + "," + str(end.month))
     else:
         month = (start.month)
 
 
 # Raw sql requirement
     data = db.session.execute(
-        f"SELECT m.id, m.date, coalesce(m.moodrating, 0) as moodrating, m.comment, c.day_of_week FROM mood m RIGHT JOIN calendar c ON m.date=c.day_id WHERE c.week_of_year={week} AND c.day_of_week <> 0 AND month IN ({month}) AND year={dt.year} UNION (SELECT m.id, m.date, coalesce(m.moodrating, 0) AS moodrating, m.comment, c.day_of_week FROM mood m RIGHT JOIN calendar c on m.date=c.day_id WHERE c.week_of_year={week - 1} AND c.day_of_week={0} AND c.month IN ({month}) AND c.year={dt.year}) ORDER BY day_of_week")
+        f"SELECT m.id, m.date, coalesce(m.moodrating, 0) as moodrating, m.comment, c.day_of_week FROM mood m RIGHT JOIN calendar c ON m.date=c.day_id WHERE c.week_of_year={week} AND c.day_of_week <> 0 AND month IN ({month}) AND year={dt.year} UNION (SELECT m.id, m.date, coalesce(m.moodrating, 0) AS moodrating, m.comment, c.day_of_week FROM mood m RIGHT JOIN calendar c on m.date=c.day_id WHERE c.week_of_year={week - 1} AND c.day_of_week={0} AND c.month IN ({month}) AND c.year={dt.year}) ORDER BY day_of_week"
+    )
 
     return jsonify({'result': [dict(row) for row in data]})
 
@@ -123,7 +150,7 @@ def get_weeks_moods():
 def add_mood():
     date = request.form['date']
     moodrating = request.form['moodrating']
-    moodratingDivided = int(moodrating)/10
+    moodratingDivided = int(moodrating) / 10
     comment = request.form['comment']
 
     if date == '' or moodrating == '':
@@ -131,7 +158,7 @@ def add_mood():
         return redirect('/moodForm')
 
     exists = db.session.query(db.exists().where(Mood.date == date)).scalar()
-    if(exists):
+    if (exists):
         flash("You've already entered a mood for this date.")
         return redirect('/moodForm')
     else:
